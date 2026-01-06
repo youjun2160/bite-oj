5d45a7a17ea896b7e830e0c640a5d23049378eb0
youjun2160 <2056992259@qq.com>
2026-01-06 14:27:53 +0800

    first commit

---
+++ oj-modules/oj-friend/src/main/java/com/bite/Main.java
@@ -0,0 +1,7 @@
+package com.bite;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ oj-modules/oj-judge/src/main/java/com/bite/judge/OjJudgeApplication.java
@@ -0,0 +1,13 @@
+package com.bite.judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class OjJudgeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OjJudgeApplication.class, args);
+    }
+}
